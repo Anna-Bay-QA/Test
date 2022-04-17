37be8ecff5ed0c116c27c781a8047f895efe07dd
Anna Bay <anna.bay.qa@gmail.com>
2022-04-17 11:55:12 -0800

    SSH key

---
+++ testFile.js
@@ -0,0 +1,13 @@
+My first test file to GIT_Hub
+
+
+Generating a new SSH key and adding it to the ssh-agent
+
+1. Open Git Bash.
+
+2. Paste the text below, substituting in your GitHub email address:
+
+$ ssh-keygen -t ed25519 -C "your_email@example.com"
+
+
+Add the SSH key to your account on GitHub. For more information, see "Adding a new SSH key to your GitHub account."
